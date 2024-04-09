069d4c8354b1793ada113cd6ac3a44c90b7d5db2
Klaudia Góralska <klaudia.goralska95@gmail.com>
2024-04-09 18:15:09 +0200

    Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      azure: {
+        400: '#73BAFB',
+        500: '#3A90F7'
+      },
+      'french-gray': {
+        200: '#DCDFE9',
+        400: '#AEB0C9',
+        700: '#747191'
+      },
+      space: {
+        950: '#1A1D46'
+      },
+    },
+    extend: {
+      margin: {
+        '20px': '20px',
+        '32px': '32px',
+        '113px': '113px',
+      },
+      padding: {
+        '216px': '216px',
+        '113px': '113px',
+      }
+    },
+  },
+  plugins: [],
+}
+
